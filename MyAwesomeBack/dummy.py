55cbefc8ffd293cdbf3282068143a5baff480618
Yurs <yurs.ksf1@gmail.com>
2018-08-12 07:10:59 -0500

    Merge branch 'gh-pages'
    add service

--- MyAwesomeBack/dummy.py
+++ MyAwesomeBack/dummy.py
@@ -1,18 +1,35 @@
 import json
+import random
+import os
 from apistar import App, Route
 from apistar import http
-
-import os
 
 BASE_DIR = os.path.dirname(__file__)
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
-mydataDummy = {
-    'lugar': 'medellin',
-    'magnitud': '3.5'
-
-}
+mydataDummy = [
+    {
+        'lugar': 'Bucaramanga',
+        'magnitud': round(random.uniform(2,6), 2)
+    },    
+    {
+        'lugar': 'Medellin',
+        'magnitud': round(random.uniform(2,6), 2)
+    },    
+    {
+        'lugar': 'Cali',
+        'magnitud': round(random.uniform(2,6), 2)
+    },    
+    {
+        'lugar': 'Bogotá',
+        'magnitud': round(random.uniform(2,6), 2)
+    },    
+    {
+        'lugar': 'Barranquilla',
+        'magnitud': round(random.uniform(2,6), 2)
+    },
+    ]
 
 
 def welcome(app: App, name=None):
@@ -22,14 +39,23 @@ def api():
     #content = mydataDummy
     #headers = {'Content-Type': 'text/plain'}
     #return http.Response(content, headers=headers)
-    data = mydataDummy
+    data = mydataDummy[random.randint(0,4)]
     return data
+    
+def map():
+    return app.render_template('col_2.svg')
+
+
+def map():
+    return app.render_template('col_2.svg')
+
     
 
 
 routes = [
     Route('/', method='GET', handler=welcome),
     Route('/api', method='GET', handler=api),
+    Route('/map', method='GET', handler=map),
 ]
 
 app = App(routes=routes, template_dir=TEMPLATE_DIR, static_dir=STATIC_DIR)
